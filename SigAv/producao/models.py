dc348cd01063649fec06e710c5836805de4aac4e
OBO13 <onildobr@gmail.com>
2021-04-13 10:22:52 -0300

    criação tabelas
    tabelas

--- SigAv/producao/models.py
+++ SigAv/producao/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+TIPODECRIACAO = [
+    ('A', 'X'),
+    ('B', 'Y'),
+    ('C', 'Z'),
+]
+
+STATUS = [
+    ('A', 'Isolamento'),
+    ('B', 'Liberados'),
+    ('C', 'Finalizado'),
+]
+
+class Fase_postura(models.Model):
+    
+    #fk_lote = models.ForeignKey(Lote, on_delete=models.RESTRICT)
+    tipo_sistema = models.CharField(max_length=100, choices=TIPODECRIACAO)
+    data_chegada = models.DateField(default=date.today)
+    quantidade_aves_chegada = models.PositiveIntegerField()
+    quantidade_aves_final = models.PositiveIntegerField(null=True, blank=True)
+    observacoes = models.CharField(max_length=400)
+
+
+
+class Movimento_diario_postura(models.Model):
+    
+    
+    fk_fase_postura = models.ForeignKey(Fase_postura, on_delete=models.RESTRICT)
+    data = models.DateField(default=date.today)
+    mortalidade = models.PositiveIntegerField()
+    primeira_coleta = models.PositiveIntegerField()
+    segunda_coleta = models.PositiveIntegerField()
+    ovos_quebrados = models.PositiveIntegerField()
+    percentual_postura = models.PositiveIntegerField()
+       
+
+  
--- SigAv/vacina/models.py
+++ SigAv/vacina/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+
+APLICACAO = [
+    ('A', 'X'),
+    ('B', 'Y'),
+    ('C', 'Z'),
+]
+
+ADMINISTRACAO = [
+    ('A', 'X'),
+    ('B', 'Y'),
+    ('C', 'Z'),
+]
+
+class Vacina(models.Model):
+    
+    tipo = models.CharField(max_length=100)
+    doses = models.CharField(max_length=50, choices=APLICACAO)
+
+
+
+
+class Aplicacao_vacina(models.Model):
+    
+    fk_vacina = models.ForeignKey(Vacina, on_delete=models.RESTRICT)
+    #fk_lote = models.ForeignKey(Lote, on_delete=models.RESTRICT)
+    data = models.DateField(default=date.today)
+    observacoes = models.CharField(max_length=400)
+      
+
+
+
+class Tempo_vacinacao(models.Model):
+    
+    fk_vacina = models.ForeignKey(Vacina, on_delete=models.RESTRICT)
+    via_administracao = models.CharField(max_length=100, choices=ADMINISTRACAO)
+    periodo_administracao = models.CharField(max_length=100)
+      
+
