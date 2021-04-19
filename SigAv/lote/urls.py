36ddc4e7425e9ab25e18dbe1f0c695ee0e4e9281
Carla Fernandes <carlafcf@gmail.com>
2021-04-19 11:19:40 -0300

    Modelo CRUD no model LOTE

--- SigAv/lote/urls.py
+++ SigAv/lote/urls.py
@@ -5,10 +5,9 @@ from . import views
 app_name = 'lote'
 
 urlpatterns = [
-    path('criar/', views.CriarLote.as_view(), name='create'),
+    path('criar/', views.CriarLote.as_view(), name='criar'),
     path('listar/', views.ListarLotes.as_view(), name='listar'),
-    # path('deletar/', include('lote.urls')),
-    # path('editar/'),
-    # path('vacinar/'),
-    # path('realocar/')
+    path('editar/<int:pk>', views.EditarLote.as_view(), name='editar'),
+    path('deletar/<int:pk>', views.DeletarLote.as_view(), name='deletar'),
+    path('detalhes/<int:pk>', views.DetalharLote.as_view(), name='detalhes'),
 ]
--- SigAv/lote/views.py
+++ SigAv/lote/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView, DetailView
 from .models import Lote
 
 from django.db import connection
 
 class CriarLote(CreateView):
     model = Lote
-    fields = ['localidade', 'data_chegada', 'quantidade_inicial']
-    template_name = 'lote/criar_lote.html'
+    fields = ['codigo', 'data_chegada', 'localidade', 'aptidao',
+                'quantidade_aves_chegada', 'raca']
+    template_name = 'lote/criar.html'
     # form
 
     def get_success_url(self):
@@ -17,6 +18,25 @@ class CriarLote(CreateView):
 
 class ListarLotes(ListView):
     model = Lote
-    template_name = 'lote/listar_lotes.html'
+    template_name = 'lote/listar.html'
     # lote_list
+
+class EditarLote(UpdateView):
+    model = Lote
+    fields = ['data_chegada', 'localidade', 'aptidao',
+                'quantidade_aves_chegada', 'raca']
+    template_name = 'lote/editar.html'
+    success_url = reverse_lazy('lote:listar')
+    # form
+
+class DeletarLote(DeleteView):
+    model = Lote
+    template_name = 'lote/confirmacao_deletar.html'
+    success_url = reverse_lazy('lote:listar')
+    # object
+
+class DetalharLote(DetailView):
+    model = Lote
+    template_name = 'lote/detalhes.html'
+    # object
 
