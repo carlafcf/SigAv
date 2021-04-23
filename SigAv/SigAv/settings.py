5e07bbb54dbe729ca4ca57a0fe0e7d1ef60ed9cd
Carla Fernandes <carlafcf@gmail.com>
2021-04-23 09:13:27 -0300

    Criação do modelo de TEMPLATE

--- SigAv/SigAv/settings.py
+++ SigAv/SigAv/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIR = STATIC_DIR
+STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
