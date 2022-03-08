e090f2fbe14e0e2b8e6dbdef6da001baac66a4fd
pedrofnsc1 <pedro.ricardo575@gmail.com>
2022-03-08 11:53:37 -0300

    App vacina - Criar (apenas admin) e listar feitos

--- SigAv/vacina/admin.py
+++ SigAv/vacina/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Vacina, Aplicacao_vacina, Tempo_vacinacao
+from .models import Vacina, AplicacaoVacina, TempoVacinacao
 
 admin.site.register(Vacina)
-admin.site.register(Aplicacao_vacina)
-admin.site.register(Tempo_vacinacao)
+admin.site.register(AplicacaoVacina)
+admin.site.register(TempoVacinacao)
--- SigAv/vacina/models.py
+++ SigAv/vacina/models.py
@@ -1,52 +1,56 @@
 from django.db import models
 from datetime import date
 from lote.models import Lote #importa a classe que stá sendo referênciada através da chave estrangeira
+ 
+# essas aplicações (tipos de vacina), foram inseridas aqui baseadas do 
+# no site da embarapa 
+# link: https://www.agencia.cnptia.embrapa.br/gestor/frango_de_corte/arvore/CONT000fc6gr40002wx5eo0a2ndxypswho8b.html
 
 APLICACAO = [
-    ('A', 'X'),
-    ('B', 'Y'),
-    ('C', 'Z'),
+
+    ('A', 'Doença de Marek'),
+    ('B', 'Doença de Gumboro'),
+    ('C', 'Varíola Aviária'),
+    ('D', 'Bronquite infecciosa das Galinhas'),
+    ('E', 'Doença de Newcastle')
 ]
 
 ADMINISTRACAO = [
-    ('A', 'Oral'),
-    ('B', 'Y'),
-    ('C', 'Z'),
-]
 
+    ('A', 'Oral'),
+    ('B', 'Injetavél'),
+    ('C', 'Membrana da Asa'),
+]
 class Vacina(models.Model):
     
-    tipo = models.CharField(max_length=100)
-    # doses = models.CharField(max_length=50, choices=APLICACAO)
-    doses = models.PositiveIntegerField()
+    tipo  = models.CharField(max_length=1, choices=APLICACAO)
+    doses = models.CharField(max_length=1, choices=ADMINISTRACAO)
 
     def __str__(self):
         return self.tipo
-    
+        
     class Meta:
-        ordering = ('tipo',)
+        ordering = ('tipo', 'doses')
 
-class Aplicacao_vacina(models.Model):
+class AplicacaoVacina(models.Model):
     
-    vacina = models.ForeignKey(Vacina, on_delete=models.RESTRICT)
-    lote = models.ForeignKey(Lote, on_delete=models.RESTRICT)
+    id_vacina = models.ForeignKey   (Vacina, on_delete=models.RESTRICT)
+    id_lote = models.ForeignKey(Lote, on_delete=models.RESTRICT)
     data = models.DateField(default=date.today)
     observacoes = models.CharField(max_length=400, null=True, blank=True)
       
     def __str__ (self):
-        return str(self.vacina) + " - " + str(self.lote)
-
+        return str(self.vacina) + " - " + str(self.id_lote)
     class Meta:
-        ordering = ('lote','vacina',)
+        ordering = ('data','observacoes')
 
-class Tempo_vacinacao(models.Model):
+class TempoVacinacao(models.Model):
     
-    vacina = models.ForeignKey(Vacina, on_delete=models.RESTRICT)
+    id_vacina = models.ForeignKey(Vacina, on_delete=models.RESTRICT)
     via_administracao = models.CharField(max_length=1, choices=ADMINISTRACAO)
     periodo_administracao = models.CharField(max_length=100)
 
     def __str__(self):
-        return str(self.vacina)
-    
+        return str(self.via_administracao) 
     class Meta:
-        ordering= ('vacina', 'periodo_administracao',)
+        ordering= ('via_administracao', 'periodo_administracao')
